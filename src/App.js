563c9f0e75573530d89a4edd59af90487af08fe4
ShuvenduSamanta <shuvendu247@gmail.com>
2020-09-24 12:25:03 +0530

    create api & fetch data from url

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
-import styles from './App.module.css';
-
 import { Cards, Chart, CountryPicker } from './components'
-/* function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Shuvendu Samanta React with harry
-        </a>
-      </header>
-    </div>
-  );
-}  */
-class App extends React.Component{
-  render(){
-    return(
+import styles from './App.module.css';
+import { fetchData } from './api'
+
+class App extends React.Component {
+  state = {
+    data: {},
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({data: fetchedData});
+  }
+  render() {
+    const { data } = this.state;
+    return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data} />
         <Chart />
         <CountryPicker />
         <h1>App</h1>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Card, Cardcontent, Typography, Grid } from '@material-ui/core';
+import styles from './Cards.modules.css'
+const Cards = (props) => {
+    console.log(props);
 
-const Cards = () => {
+
     return(
-        <h1>Cards</h1>
+        <div className={styles.container}>
+             {/* <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                 <Cardcontent>
+                     <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                     <Typography variant="h5">REAL DATA</Typography>
+                     <Typography color="textSecondary">REAL DATE</Typography>
+                     <Typography variant="body2">Number of active cases of COVID-19</Typography>
+                     </Cardcontent>   
+                </Grid>
+            </Grid>  */}
+        </div>
     )
 }
 
